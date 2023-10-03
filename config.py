96fef9ebb5da2c1a3077fec1ef0eb7dc8f8acaf8
Joshua Caponigro <jcaponigro20@gmail.com>
2023-10-03 13:27:16 -0400

    config and updates

---
+++ config.py
@@ -0,0 +1,14 @@
+PING_TIMEOUT: int = 180
+ERROR_LOG_NAME: str = "logs/error.log"
+INFO_LOG_NAME: str = "logs/info.log"
+TEST_URLS: dict = {
+        'django-backend': [
+            "https://scanbandz.com",
+            "https://scanbandz.com/host",
+        ],
+        'angular-ticket-frontend': [
+            "https://tickets.scanbandz.com",
+            "https://tickets.scanbandz.com/event/32",
+            "https://tickets.scanbandz.com/donate/32",
+        ]
+    }
--- main.py
+++ main.py
@@ -1,69 +1,85 @@
-
-import time
-import requests
 import logging
+import socket
 import sys
+import time
+
+import requests
+
+from config import ERROR_LOG_NAME, INFO_LOG_NAME, PING_TIMEOUT, TEST_URLS
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-# Create console handler and set level to INFO
+# Formatter
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+# Console handler
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.INFO)
-
-# Create formatter
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-# Add formatter to handler
 handler.setFormatter(formatter)
-
-# Add handler to logger
 logger.addHandler(handler)
+
+# Log handler
+file_handler = logging.FileHandler(INFO_LOG_NAME)
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+# Error handler
+file_handler = logging.FileHandler(ERROR_LOG_NAME)
+file_handler.setLevel(logging.ERROR)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 class HealthChecker:
-    TEST_ENDPOINTS: list[dict] = [
-        {'django-backend': [
-            "https://scanbandz.com",
-            "https://scanbandz.com/host",
-        ]},
-        {'angular-ticket-frontend': [
-            "https://tickets.scanbandz.com",
-            "https://tickets.scanbandz.com/event/32",
-        ]}
-    ]
+    @staticmethod
+    def run() -> None:
+        logger.info(
+            "Starting health check at %s", time.strftime("%H:%M:%S", time.localtime())
+        )
+        for key, value in TEST_URLS.items():
+            logger.info("Checking %s...", key)
+            for url in value:
+                if HealthChecker.ping(url):
+                    logger.info("Endpoint %s is up!", url)
+                else:
+                    logger.error("Endpoint %s is down!", url)
+            logger.info("Finished checking %s.", key)
+        logger.info(
+            "Finished health check at %s", time.strftime("%H:%M:%S", time.localtime())
+        )
 
-    def run(self) -> None:
-        print("\n\n")
-        logger.info("Starting health check at " +
-                    time.strftime("%H:%M:%S", time.localtime()))
-        for endpoint in self.TEST_ENDPOINTS:
-            for key, value in endpoint.items():
-                logger.info('\033[34m' + f"Checking {key}..." + '\033[0m')
-                for url in value:
-                    if self.ping_page(url):
-                        logger.info(
-                            '\033[32m' + f"Endpoint {url} is up!" + '\033[0m')
-                    else:
-                        self.handle_issue(url)
-                logger.info('\033[34m' + f"Finished checking {key}." + '\033[0m')
-        logger.info("Finished health check at " +
-                    time.strftime("%H:%M:%S", time.localtime()))
-
-    def ping_page(self, url: str) -> bool:
+    @staticmethod
+    def ping(url: str) -> bool:
         try:
             response = requests.get(url)
             return response.status_code == 200
-        except:
+        except requests.RequestException:
             return False
 
-    def handle_issue(self, url: str) -> None:
-        logger.error('\033[31m' + f"Endpoint {url} is down!" + '\033[0m')
-        with open("errors.txt", "a") as f:
-            f.write(f"Endpoint {url} is down!\n")
+    @staticmethod
+    def is_connected() -> bool:
+        """Check if the device is connected to the internet."""
+        try:
+            socket.create_connection(("8.8.8.8", 53))
+            logger.info("Connected to Wi-Fi.")
+            return True
+        except OSError:
+            logger.error("Not connected to Wi-Fi.")
+        return False
 
 
 if __name__ == "__main__":
     while True:
-        HealthChecker().run()
-        time.sleep(60)
+        if HealthChecker.is_connected():
+            HealthChecker.run()
+        else:
+            logger.error("Lost Wi-Fi connectivity. Stopping health checks.")
+            should_resume: str = input("Would you like to resume health checks? (y/n) ")
+            if should_resume.lower() == "y":
+                logger.info("Resuming health checks.")
+            else:
+                logger.info("Exiting.")
+                sys.exit()
+        time.sleep(PING_TIMEOUT)
